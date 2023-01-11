9c16036f300692b8f588e66deea17b439d8be150
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2023-01-12 01:37:30 +0600

    about section added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-100">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://i.ibb.co/Zd90Tz7/menufacturer.jpg"
+            className="max-w-md rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">About MediQuick</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi. Provident cupiditate voluptatem et in. Quaerat
+              fugiat ut assumenda excepturi exercitationem quasi. In deleniti
+              eaque aut repudiandae et a id nisi. Provident cupiditate
+              voluptatem et in. Quaerat fugiat ut assumenda excepturi
+              exercitationem quasi. In deleniti eaque aut repudiandae et a id
+              nisi. Provident cupiditate voluptatem et in. Quaerat fugiat ut
+              assumenda excepturi exercitationem quasi. In deleniti eaque aut
+              repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-secondary">Read More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
