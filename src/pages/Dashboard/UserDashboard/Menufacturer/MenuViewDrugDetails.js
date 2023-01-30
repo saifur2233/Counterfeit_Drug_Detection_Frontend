12280c36024d09492fed9498eae13b112be5ecf0
MD SAIFUR RAHMAN <saifurnstuiit223344@gmail.com>
2023-01-31 00:37:28 +0600

    menufacturer view all drugs

--- src/pages/Dashboard/UserDashboard/Menufacturer/MenuViewDrugDetails.js
+++ src/pages/Dashboard/UserDashboard/Menufacturer/MenuViewDrugDetails.js
@@ -1,6 +1,29 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../../../shared/Loading/Loading";
 
 const MenuViewDrugDetails = () => {
+  const {
+    data: allDrugs = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["allDrugs"],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:4000/api/v1/menufacturer/drugs`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  console.log(allDrugs);
+
   return (
     <div>
       <div>
@@ -12,88 +35,48 @@ const MenuViewDrugDetails = () => {
             <table className="table table-zebra w-full">
               <thead>
                 <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>Job</th>
-                  <th>Favorite Color</th>
+                  <th>Index</th>
+                  <th>menufacturerName</th>
+                  <th>drugName</th>
+                  <th>drugCode</th>
+                  <th>drugDosage</th>
+                  <th>drugQuantity</th>
+                  <th>mfgDate</th>
+                  <th>expDate</th>
                   <th>Action</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th>1</th>
-                  <td>Cy Ganderton</td>
-                  <td>Quality Control Specialist</td>
-                  <td>Blue</td>
-                  <td>
-                    <button className="btn btn-square btn-secondary btn-outline">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-
-                <tr>
-                  <th>2</th>
-                  <td>Hart Hagerty</td>
-                  <td>Desktop Support Technician</td>
-                  <td>Purple</td>
-                  <td>
-                    <button className="btn btn-square btn-secondary btn-outline">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
-
-                <tr>
-                  <th>3</th>
-                  <td>Brice Swyre</td>
-                  <td>Tax Accountant</td>
-                  <td>Red</td>
-                  <td>
-                    <button className="btn btn-square btn-secondary btn-outline">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
-                  </td>
-                </tr>
+                {allDrugs.drugs.map((drug, i) => (
+                  <tr key={drug._id}>
+                    <th>{i + 1}</th>
+                    <td>{drug.menufacturerName}</td>
+                    <td>{drug.drugName}</td>
+                    <td>{drug.drugCode}</td>
+                    <td>{drug.drugDosage}</td>
+                    <td>{drug.drugQuantity}</td>
+                    <td>{drug.mfgDate}</td>
+                    <td>{drug.expDate}</td>
+                    <td>
+                      <button className="btn btn-square btn-secondary btn-outline">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
---
+++ src/shared/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div class="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
+      <div class="border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-64 w-64"></div>
+    </div>
+  );
+};
+
+export default Loading;
